b50fef5e541896d6d5c879bcc9f96512c5d7bf81
Xin-Xin Wang - dumpling <get@xin-xin.me>
2020-12-06 02:16:30 -0500

    Add ContinuableRealNum

---
+++ src/modules/calc/ContinuableRealNum.js
@@ -0,0 +1,55 @@
+// @flow
+
+import RealNum from './RealNum';
+import Precision, { RegularPrec, InfPrec } from './Precision';
+
+export default class ContinuableRealNum {
+  initialized: boolean = false;
+  prevLastDig: number;
+  prevPrec: Precision;
+
+  init(a: RealNum): void {
+    if (this.initialized) return;
+    this.initialized = true;
+    this.prevLastDig = 0;
+    this.prevPrec = new RegularPrec(0 - a.sizeNonZero());
+  }
+
+  // given an updated RealNum and the precision of the update
+  // returns the diff from the previous number as
+  // [diffAtPrec, diffAfterPrec]
+  // diffAtPrec is either 0, 1, or -1 depending on the sign of
+  // the number
+  cont(a: RealNum, prec: Precision): [RealNum, RealNum] {
+    if (a.isZero()) {
+      // prevent starting from 0 since otherwise
+      // we can't guarantee the sign won't change
+      // and that previous digits won't change
+      // by too much
+      throw new Error('ContinuableRealNum values cannot be 0');
+    }
+
+    this.init(a);
+
+    if (prec.le(this.prevPrec)) {
+      throw new Error('ContinuableRealNum new prec must be > prev prec');
+    }
+
+    const [aLastDig, diffAfterPrec] = a.getNumAfterPrec(this.prevPrec);
+
+    let diff = 0;
+    if (aLastDig !== this.prevLastDig) {
+      diff = 1;
+    }
+
+    const diffNum = RealNum.digitAtPrec(!a.pos, diff, this.prevPrec);
+
+    this.prevPrec = prec;
+
+    if (prec !== InfPrec) {
+      this.prevLastDig = a.getDigitAtPrec(prec);
+    }
+
+    return [diffNum, diffAfterPrec];
+  }
+}
---
+++ src/modules/calc/ContinuableRealNum.test.js
@@ -0,0 +1,110 @@
+// @flow
+
+import ContinuableRealNum from './ContinuableRealNum';
+import RealNum from './RealNum';
+import Precision, { RegularPrec, InfPrec } from './Precision';
+
+test('initializes correctly', () => {
+  const contNum = new ContinuableRealNum();
+
+  const [diffAtPrec, diffAfterPrec] = contNum.cont(
+    RealNum.fromStr('1'),
+    new RegularPrec(0),
+  );
+  expect(diffAtPrec).toObjEqual(RealNum.zero);
+  expect(diffAfterPrec).toObjEqual(RealNum.fromStr('1'));
+  expect(contNum.prevLastDig).toBe(1);
+  expect(contNum.prevPrec).toObjEqual(new RegularPrec(0));
+});
+
+test('throws when repeating precisions', () => {
+  const contNum = new ContinuableRealNum();
+
+  const [diffAtPrec, diffAfterPrec] = contNum.cont(
+    RealNum.fromStr('1'),
+    new RegularPrec(0),
+  );
+  expect(diffAtPrec).toObjEqual(RealNum.zero);
+  expect(diffAfterPrec).toObjEqual(RealNum.fromStr('1'));
+
+  expect(() => {
+    contNum.cont(RealNum.fromStr('1'), new RegularPrec(0));
+  }).toThrow('ContinuableRealNum new prec must be > prev prec');
+});
+
+const sequences = [
+  [
+    [
+      ['1', new RegularPrec(0), '0', '1'],
+      ['0.4', new RegularPrec(1), '-1', '0.4'],
+      ['0.49', new RegularPrec(2), '0', '0.09'],
+      ['0.49', new RegularPrec(3), '0', '0'],
+      ['0.4891', new RegularPrec(4), '-0.001', '0.0001'],
+      ['0.4891', new RegularPrec(5), '0', '0'],
+      ['0.4891', InfPrec, '0', '0'],
+    ],
+  ],
+  [
+    [
+      ['2', new RegularPrec(0), '0', '2'],
+      ['1.5', new RegularPrec(1), '-1', '0.5'],
+      ['1.59', new RegularPrec(2), '0', '0.09'],
+      ['1.595', new RegularPrec(3), '0', '0.005'],
+      ['1.595', new RegularPrec(4), '0', '0'],
+      ['1.595', new RegularPrec(5), '0', '0'],
+      ['1.5950001', InfPrec, '0', '0.0000001'],
+    ],
+  ],
+  [
+    [
+      ['1', new RegularPrec(0), '0', '1'],
+      ['0.01', InfPrec, '-1', '0.01'],
+    ],
+  ],
+  [
+    [
+      ['1', new RegularPrec(0), '0', '1'],
+      ['0.09', InfPrec, '-1', '0.09'],
+    ],
+  ],
+  [
+    [
+      ['-1', new RegularPrec(0), '0', '-1'],
+      ['-0.4', InfPrec, '1', '-0.4'],
+    ],
+  ],
+  [
+    [
+      ['-2', new RegularPrec(0), '0', '-2'],
+      ['-1.5', InfPrec, '1', '-0.5'],
+    ],
+  ],
+  [
+    [
+      ['0.4891', new RegularPrec(5), '0', '0.4891'],
+      ['0.4891', InfPrec, '0', '0'],
+    ],
+  ],
+  [
+    [
+      ['1.595', new RegularPrec(5), '0', '1.595'],
+      ['1.5950001', InfPrec, '0', '0.0000001'],
+    ],
+  ],
+];
+
+test.each(sequences)(
+  'works with the given sequence %#',
+  (seq: Array<[string, Precision, string, string]>) => {
+    const contNum = new ContinuableRealNum();
+
+    for (const [a, prec, expDiffAtPrec, expDiffAfterPrec] of seq) {
+      const [diffAtPrec, diffAfterPrec] = contNum.cont(
+        RealNum.fromStr(a),
+        prec,
+      );
+      expect(diffAtPrec.toString()).toBe(expDiffAtPrec);
+      expect(diffAfterPrec.toString()).toBe(expDiffAfterPrec);
+    }
+  },
+);
